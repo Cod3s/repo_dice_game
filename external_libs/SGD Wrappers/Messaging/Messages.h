59337a2c7d42fca1e99b3989d01665d76436e6df
Alex Gendron <cod3s3@gmail.com>
2015-09-02 01:30:19 -0700

    Actually adding SGD files

---
+++ external_libs/SGD Wrappers/Messaging/Messages.h
@@ -0,0 +1,21 @@
+#pragma once
+
+typedef int MSGID;
+
+enum eMsgTypes { MSG_NULL = 0, MSG_MAX };
+
+class CBaseMessage
+{
+private:
+	MSGID	m_msgID;
+
+public:
+	CBaseMessage(MSGID msgID)
+	{
+		m_msgID = msgID;
+	}
+
+	virtual ~CBaseMessage(void) {}
+
+	MSGID GetMsgID(void)	{ return m_msgID; }
+};
